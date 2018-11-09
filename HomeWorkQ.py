e5ee2c7466e4a2657006a98d77c2c9eb34350f76
shadibdair <43513994+shadibdair@users.noreply.github.com>
2018-11-09 21:31:24 +0200

    py

--- HomeWorkQ.py
+++ HomeWorkQ.py
@@ -1,4 +1,4 @@
-#HW by Shadi Bdair
+#HW by Shadi Bdair.
 num=int(input("Enter a number: "))
 num2words = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', \
              6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', \
